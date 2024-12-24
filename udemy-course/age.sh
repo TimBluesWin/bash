799f5a9a6105cbe08493d5ab89f410af1abb76d1
Timothy Aditya Sutantyo <timisutantyo@gmail.com>
2024-12-24 19:53:38 +0100

    Add files that I've done in the Udemy course to the folder

---
+++ udemy-course/age.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+read -p "Enter your age: " age
+
+if [[ $age -lt 18 ]] && [[ $age -ge 0 ]]
+then
+	echo "You are minor!"
+elif [[ $age -eq 18 ]]
+then
+	echo "Congratulations, you've just become major!"
+elif [[ $age -gt 18 ]] && [[ $age -le 100 ]]
+then
+	echo "You are major"
+else
+	echo "Invalid age!"
+fi
---
+++ udemy-course/first_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p dir1
+# This is a Bash comment
+echo "some text" > dir1/file.txt
+ls -l dir1 # listing the directory
+cat dir1/file.txt    # file contents
